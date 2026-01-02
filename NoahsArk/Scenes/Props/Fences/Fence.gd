64460d71279ccfd8da9b320d6051a239af4e261a
DKRapArtist <167987183+DKRapArtist@users.noreply.github.com>
2026-01-02 21:34:19 +0200

    fences no fade

--- NoahsArk/Scenes/Props/Fences/Fence.gd
+++ NoahsArk/Scenes/Props/Fences/Fence.gd
@@ -2,7 +2,6 @@ extends Node2D
 class_name BaseFence
 
 @onready var top: Sprite2D = $Top
-@onready var trigger: Area2D = $FadeTrigger
 
 const FADE_ALPHA := 0.35
 const FADE_SPEED := 8.0
@@ -10,8 +9,7 @@ const FADE_SPEED := 8.0
 var target_alpha := 1.0
 
 func _ready() -> void:
-	trigger.body_entered.connect(_on_body_entered)
-	trigger.body_exited.connect(_on_body_exited)
+	top.modulate.a = 1.0
 
 func _process(delta: float) -> void:
 	top.modulate.a = lerp(top.modulate.a, target_alpha, delta * FADE_SPEED)
